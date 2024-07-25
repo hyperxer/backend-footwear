44e11f068a998488c52454ab196044f25334dceb
hyperxer <hyperxer789@gmail.com>
2024-07-25 17:21:07 +0700

    +

--- Middleware/upload.middleware.js
+++ Middleware/upload.middleware.js
@@ -4,11 +4,11 @@ const path = require("path");
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'imgproducts/'); // กำหนดโฟลเดอร์สำหรับเก็บรูปภาพ
+    cb(null, 'imgproducts/');
   },
   filename: (req, file, cb) => {
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-    cb(null, uniqueSuffix + path.extname(file.originalname)); // ตั้งชื่อไฟล์ด้วย uniqueSuffix
+    cb(null, uniqueSuffix + path.extname(file.originalname));
   }
 });
 
